7316752ff0dc99d355de9c26bc986eb618604f0b
Kritsaphong Phuthibpaphaisit <kritsaphong.phu@stu.nida.ac.th>
2022-04-05 14:17:04 +0700

    ex1_hello_console

---
+++ csharp_basic/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static csharp_basic.ex1_hello_console;
+
+
+namespace csharp_basic
+{
+    class Program
+    {
+        static void Main()
+            {
+                ex1();
+
+            }
+    }
+}
+
---
+++ csharp_basic/ex1_hello_console.cs
@@ -0,0 +1,19 @@
+using System;
+namespace csharp_basic
+{
+    public class ex1_hello_console
+    {
+        public static void ex1(){
+            Console.WriteLine("Hello, World! ");
+            Console.WriteLine("Hello, World!");
+            //Make sound
+            Console.Write("sound \a");
+            //Tab
+            Console.Write("\t tab");
+            //New-line
+            Console.Write("\nnewline");
+
+
+        }
+    }
+}
